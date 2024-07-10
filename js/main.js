b60e9b9af43b5b0a0c696ad579d81279b3493a40
Matias <matypal10@gmail.com>
2024-07-09 21:00:44 -0300

    2da preentrega

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
+// <---------------- 1era preentrega ---------------->
+
 //mensaje de bienvenida
+/* 
 alert("Bienvenido a tu simulador de prestamos de confianza\nEl interes mensual que manejamos es del 4% y aceptamos hasta 60 meses")
 
 let edad = parseInt(prompt("ingrese su edad:"));
@@ -20,3 +24,43 @@ while (edad >= 18){
 function cuotasPrestamo(capital, meses, interesMensual){
     return ((interesMensual/100)*capital)/(1-(1+(interesMensual/100))**-meses);
 };
+*/
+
+// <---------------- 2da preentrega ----------------> 
+// Función para crear un nuevo libro
+function createBook() {
+    let title = prompt("Ingrese el título del libro:");
+    let author = prompt("Ingrese el autor del libro:");
+    let year = prompt("Ingrese el año de publicación:");
+
+    // Objeto libro
+    let book = {
+        title: title,
+        author: author,
+        year: parseInt(year),
+        getSummary: function() {
+        return `${this.title} fue escrito por ${this.author} en ${this.year}.`;
+    }
+    };
+    return book;
+}
+// Array para almacenar los libros
+let books = [];
+
+// Función para añadir libros al array
+function addBooks() {
+    let numberOfBooks = prompt("¿Cuántos libros desea ingresar?");
+    for (let i = 0; i < numberOfBooks; i++) {
+        let newBook = createBook();
+        books.push(newBook);
+    }
+}
+
+// Función para mostrar los libros ingresados
+function showBooks() {
+    let summaries = books.map(book => book.getSummary()).join("\n");
+    alert(`Resumen de libros:\n${summaries}`);
+}
+
+addBooks();
+showBooks();
